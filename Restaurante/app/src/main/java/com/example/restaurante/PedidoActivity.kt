dda662eb2e880ca0829c383d0e180686dcc972f0
Thigas77 <235085.aluno@iserj.edu.br>
2024-05-09 15:56:53 -0300

    PedidoActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
@@ -7,12 +7,16 @@ import com.example.restaurante.databinding.ActivityPedidoBinding
 class PedidoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPedidoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         binding = ActivityPedidoBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        
         setContentView(binding.root)
 
         val i = intent
+
         val quantidadePizza = i.getStringExtra("quantidadePizza").toString().toInt()
         val quantidadeSalada = i.getStringExtra("quantidadeSalada").toString().toInt()
         val quantidadeHamburguer = i.getStringExtra("quantidadeHamburguer").toString().toInt()
@@ -22,7 +26,10 @@ class PedidoActivity : AppCompatActivity() {
                 "Salada: $quantidadeSalada Preço: ${quantidadeSalada*10}\n" +
                 "Hamburguer: $quantidadeHamburguer Preço: ${quantidadeHamburguer*12}\n"
 
+
         binding.textResumo.text = texto
+    }
+}
 
     }
 }
