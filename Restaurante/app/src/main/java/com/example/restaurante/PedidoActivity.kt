8b6626b0d75a4f7c4be74b32e87caacb0bd9c114
Thigas77 <235085.aluno@iserj.edu.br>
2024-05-09 16:08:01 -0300

    PedidoActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
@@ -4,31 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.restaurante.databinding.ActivityPedidoBinding
 
-class PedidoActivity : AppCompatActivity() {
+class PedidoActivity : AppCompatActivity() { // Declaração da classe PedidoActivity que estende AppCompatActivity
 
-    private lateinit var binding: ActivityPedidoBinding
+    private lateinit var binding: ActivityPedidoBinding // Declaração da variável de layout
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        
-        binding = ActivityPedidoBinding.inflate(layoutInflater)
-        super.onCreate(savedInstanceState)
-        
-        setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) { // Método onCreate
+        binding = ActivityPedidoBinding.inflate(layoutInflater) // Infla o layout da atividade
+        super.onCreate(savedInstanceState) // Chama o método onCreate da superclasse
+        setContentView(binding.root) // Define o layout da atividade como o layout inflado
 
-        val i = intent
+        val i = intent // Obtém a intent que iniciou esta atividade
 
+        // Obtém as quantidades de cada item do pedido da intent e converte para inteiros
         val quantidadePizza = i.getStringExtra("quantidadePizza").toString().toInt()
         val quantidadeSalada = i.getStringExtra("quantidadeSalada").toString().toInt()
         val quantidadeHamburguer = i.getStringExtra("quantidadeHamburguer").toString().toInt()
 
+        // Cria uma string de resumo do pedido com base nas quantidades e preços
         val texto = "Resumo do Pedido\n" +
                 "Pizza: $quantidadePizza Preço: ${quantidadePizza*8}\n" +
                 "Salada: $quantidadeSalada Preço: ${quantidadeSalada*10}\n" +
                 "Hamburguer: $quantidadeHamburguer Preço: ${quantidadeHamburguer*12}\n"
 
+        // Define o texto do resumo do pedido na view textResumo do layout da atividade
         binding.textResumo.text = texto
     }
 }
+
 
     }
 }
